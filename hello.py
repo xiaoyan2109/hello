4fe1f3b66553173f9ea23c3beb20ce787d5ada82
xiaoyan2109 <xiaoyan2109@gmail.com>
2017-02-16 15:20:06 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True)
